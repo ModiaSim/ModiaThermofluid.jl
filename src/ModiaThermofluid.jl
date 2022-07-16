503fa744dd2b1606b1364775cf8096fe7769aa39
Clément Coïc <dr.coic@proton.me>
2022-07-16 21:24:51 +0200

    Add docstring to ModiaThermofluid.jl

--- src/ModiaThermofluid.jl
+++ src/ModiaThermofluid.jl
@@ -1,6 +1,16 @@
+"""
+    ModiaThermofluid.jl
+
+ThermoFluid library of [Modia](https://github.com/ModiaSim/Modia.jl) component models.
+(ModiaThermoFluid.jl is inspired from the Modelica library [ThermofluidStream](https://github.com/DLR-SR/ThermofluidStream) from DLR-SR).
+
+* Developer: Clément Coïc  
+* Copyright (c) 2022: DLR-SR and Clément Coïc
+* License: 3-Clause BSD License
+
+"""
 module ModiaThermofluid
 
-# Write your package code here.
 
 const path = dirname(dirname(@__FILE__))   # Absolute path of package directory
 
