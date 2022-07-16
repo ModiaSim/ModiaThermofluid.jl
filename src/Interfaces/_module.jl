fba783669e0dcf070cf929f7065fcdc6b552aa88
ClementCoic <dr.coic@proton.me>
2022-07-16 21:40:26 +0200

    Add _module.jl for ModiaThermofluid.Interfaces Following Modia3D structure: * submodules are in separate folders * `_module.jl` contains the include * separate `.jl` files for the main groups of models

---
+++ src/Interfaces/_module.jl
@@ -0,0 +1,15 @@
+"""
+    module ModiaThermoFluid.Interfaces
+
+Subset of ModiaThermoFluid containing the physical Interfaces of thermofluid components.
+
+* Developer: Clément Coïc  
+* Copyright (c) 2022: DLR-SR and Clément Coïc
+* License: 3-Clause BSD License
+
+"""
+module Interfaces
+
+using Modia
+ 
+end
